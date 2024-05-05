7f82bcdfdf771ed145f1e14364ba6dd6e1d93b61
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-06 00:22:30 +0200

    feat: Add simple UI style

---
+++ src/lib/components/License.svelte
@@ -0,0 +1,24 @@
+<p class="leading-none" xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/">
+	<a property="dct:title" rel="cc:attributionURL" href="https://forsen.wiki/">Content</a> is
+	licensed under
+	<a
+		class="underline underline-offset-4"
+		href="https://creativecommons.org/licenses/by-sa/4.0/"
+		target="_blank"
+		rel="license noopener noreferrer"
+		style="display:inline-block;"
+		>CC BY-SA 4.0<img
+			class="inline-block h-6 ml-1"
+			src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+			alt="CC"
+		/><img
+			class="inline-block h-6 ml-1"
+			src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+			alt="BY"
+		/><img
+			class="inline-block h-6 ml-1"
+			src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
+			alt="SA"
+		/></a
+	>
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,23 @@
 	<meta name="description" content="forsen" />
 </svelte:head>
 
-<section>
-	<h2>Index page</h2>
+<section class="w-full p-4">
+	<div class="m-0 w-full p-4 sm:p-8">
+		<p class="m-0 text-center leading-10">
+			forsen.wiki is currently a <strong>work in progress</strong>.
+			<br />
+			Feel free to take a look around and{' '}
+			<a
+				href="/create/"
+				class="rounded bg-fuchsia-300 p-2 font-semibold hover:bg-fuchsia-400"
+				data-sveltekit-reload>Create new article</a
+			>
+		</p>
+	</div>
+
+	<h2 class="text-2xl">Recent edits</h2>
+	<p>todo</p>
+
+	<h2 class="text-2xl">New articles</h2>
+	<p>todo</p>
 </section>
