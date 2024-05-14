224a3fe2e9114349c32c378dc9b785e9b2b5821c
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-15 00:01:22 +0200

    improve article read page

--- src/routes/w/[title]/+page.svelte
+++ src/routes/w/[title]/+page.svelte
@@ -1,19 +1,46 @@
 <script>
+	import Button from '$lib/components/Button.svelte';
+	import { EditIcon } from 'lucide-svelte';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
-<div class="container mx-auto flex grow flex-col gap-2 p-4 lg:p-0 lg:py-12">
-	<article class="prose relative max-w-[unset] grow p-2 flex flex-col">
-		<main>
+<div class="container mx-auto flex grow flex-col gap-8 p-4 lg:p-0 lg:py-12">
+	<article class="relative flex grow flex-col gap-8">
+		<header
+			class="flex w-full items-center rounded bg-gradient-to-br from-violet-200 to-violet-300 p-4"
+		>
+			<p class="grow">
+				forsen.wiki is currently a <strong>work in progress</strong>.
+			</p>
+		</header>
+
+		<main class="prose max-w-[unset] grow">
 			{@html data.html}
 		</main>
 
-		<footer class="grow flex flex-col justify-end">
-			<div class="text-purple-800">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-			<p><strong>Created:</strong> {new Date(data.post.createdTimestamp).toLocaleString()}</p>
-			{#if data.post.lastUpdated.getTime() !== data.post.createdTimestamp.getTime()}
-				<p><strong>Updated:</strong> {new Date(data.post.lastUpdated).toLocaleString()}</p>
+		<footer class="rounded bg-violet-200 p-4">
+			<p>
+				<span>
+					<strong>Created:</strong>
+					{new Date(data.post.createdTimestamp).toLocaleString()}
+				</span>
+				{#if data.post.lastUpdated.getTime() !== data.post.createdTimestamp.getTime()}
+					<span>
+						<strong>Updated:</strong>
+						{new Date(data.post.lastUpdated).toLocaleString()}
+					</span>
+				{/if}
+			</p>
+
+			{#if data.authors.length}
+				<p>
+					<span><strong>Authors:</strong></span>
+					<span>
+						{#each data.authors as author, index}{author.name}{index < data.authors.length - 1 ? ', ' : ''}{/each}
+					</span>
+				</p>
 			{/if}
 		</footer>
 	</article>
