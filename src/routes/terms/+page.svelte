c48bde33fe2d3481dfe634ca050bc78102ac718f
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-15 20:56:02 +0200

    sorry, covering

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -5,11 +5,19 @@
 		<ol>
 			<li
 				data-content="Primum non nocere."
-				class="change-me whitespace-pre relative after:absolute after:bottom-4 after:left-1 after:-z-10 after:pl-1 after:opacity-15 after:content-[attr(data-content)]"
+				class="change-me relative whitespace-pre after:absolute after:bottom-4 after:left-1 after:-z-10 after:pl-1 after:opacity-15 after:content-[attr(data-content)]"
 			>
 				Do no harm.
 			</li>
+			<li>Your account can be terminated for any reason.</li>
+			<li>Your content may be changed and/or deleted for any reason.</li>
 		</ol>
+
+		<p>
+			<strong>
+				These terms may change at any time, especially if contributors create negative controversy.
+			</strong>
+		</p>
 
 		<hr />
 
@@ -26,11 +34,5 @@
 				<strong>staff</strong>. <small>(Use another account)</small>
 			</li>
 		</ul>
-
-		<p>
-			<strong
-				>These terms may change at any time, especially if contributors create negative controversy.</strong
-			>
-		</p>
 	</section>
 </div>
