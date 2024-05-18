017141f96f8c987124d3cfba206557f81279489b
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-19 01:05:25 +0200

    use local files for cc License svgs

--- src/lib/components/License.svelte
+++ src/lib/components/License.svelte
@@ -1,32 +1,26 @@
 <script>
 	// @ts-nocheck - Complains about "xmlns:cc" and "xmlns:dct" not being valid.
+	import cc from '$lib/images/cc.svg';
+	import by from '$lib/images/by.svg';
+	import sa from '$lib/images/sa.svg';
 </script>
 
 <p
-	class="leading-none"
+	class="flex items-baseline gap-2 leading-none"
 	xmlns:cc="http://creativecommons.org/ns#"
 	xmlns:dct="http://purl.org/dc/terms/"
 >
-	<a property="dct:title" rel="cc:attributionURL" href="https://forsen.wiki/">Content</a> is
-	licensed under
+	<span>Content is licensed under</span>
 	<a
 		class="font-bold underline decoration-indigo-500 decoration-2 underline-offset-4 hover:text-indigo-300"
 		href="https://creativecommons.org/licenses/by-sa/4.0/"
 		target="_blank"
 		rel="license noopener noreferrer"
 		style="display:inline-block;"
-		>CC BY-SA 4.0<img
-			class="ml-1 inline-block h-6"
-			src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
-			alt="CC"
-		/><img
-			class="ml-1 inline-block h-6"
-			src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+		>CC BY-SA 4.0<img class="mb-1.5 ml-1 inline-block h-6" src={cc} alt="CC" /><img
+			class="mb-1.5 ml-1 inline-block h-6"
+			src={by}
 			alt="BY"
-		/><img
-			class="ml-1 inline-block h-6"
-			src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
-			alt="SA"
-		/></a
+		/><img class="mb-1.5 ml-1 inline-block h-6" src={sa} alt="SA" /></a
 	>
 </p>
