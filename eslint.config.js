17ebaa0f34e125ea6e9b179908ff01215f5c1698
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-24 18:42:40 +0200

    make eslint work

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import eslint from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type { import("eslint").Linter.Config[] } */
+export default [
+	eslint.configs.recommended,
+	prettier,
+	...svelte.configs['flat/recommended'],
+	{
+		rules: {
+			semi: ['error', 'always']
+		},
+		languageOptions: {
+			sourceType: 'module',
+			ecmaVersion: 2022,
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021
+			}
+		},
+		ignores: [
+			'.DS_Store',
+			'node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		]
+	}
+];
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import * as path from 'path';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
