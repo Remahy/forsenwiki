15251e7b9187e523c5645ae8bf368c2c149b4bb7
Karar Al-Remahy <alremahykarar@ymail.com>
2024-05-26 02:43:27 +0200

    feat: Search

---
+++ src/routes/search/+page.server.js
@@ -0,0 +1,94 @@
+import { getCacheURL } from '$lib/components/editor/validations/images.server.js';
+import prisma from '$lib/prisma.js';
+
+/**
+ * @typedef {{ type?: 'content', rawTitle: string, title: string, lastUpdated: Date }} QueryResult
+ */
+
+/**
+ * @returns {Promise<{ results: QueryResult[] }>}
+ */
+export const load = async ({ url }) => {
+	const rawQuery = url.searchParams.get('query');
+
+	if (!rawQuery) {
+		return { results: [] };
+	}
+
+	const query = rawQuery.trim();
+
+	const rawRawTitleContains = prisma.yPost.findMany({
+		where: {
+			rawTitle: {
+				contains: query,
+				mode: 'insensitive',
+			},
+		},
+		select: {
+			rawTitle: true,
+			lastUpdated: true,
+			title: true,
+		},
+	});
+
+	const rawMetadataUserName = prisma.yPost.findMany({
+		where: {
+			postUpdates: {
+				some: {
+					metadata: {
+						user: {
+							name: query.toLowerCase(),
+						},
+					},
+				},
+			},
+		},
+		select: {
+			rawTitle: true,
+			lastUpdated: true,
+			title: true,
+		},
+	});
+
+	const rawContentUserName = prisma.content.findMany({
+		where: {
+			OR: [
+				{
+					author: {
+						name: query.toLowerCase(),
+					},
+				},
+				{
+					name: {
+						contains: query.toLowerCase(),
+						mode: 'insensitive',
+					}
+				},
+			],
+		},
+	});
+
+	/** @type {QueryResult[]} */
+	let results = [];
+	const [rawTitleContains, metadataUserName, contentUserName] = await Promise.all([
+		rawRawTitleContains,
+		rawMetadataUserName,
+		rawContentUserName,
+	]);
+
+	results.push(...rawTitleContains);
+	results.push(...metadataUserName);
+
+	for (let index = 0; index < contentUserName.length; index++) {
+		const { name, hash, createdAt } = contentUserName[index];
+
+		results.push({
+			type: 'content',
+			lastUpdated: createdAt,
+			rawTitle: name,
+			title: getCacheURL(hash, name).toString(),
+		});
+	}
+
+	return { results: results.flat() };
+};
