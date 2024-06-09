923b377ab5f2a1fabf1efaeb0357efe261812cd7
Karar Al-Remahy <alremahykarar@ymail.com>
2024-06-09 23:18:28 +0200

    add VITE_DOMAIN to environment file

--- src/lib/environment/environment.js
+++ src/lib/environment/environment.js
@@ -3,3 +3,4 @@ import { IS_APPLE } from './utils';
 export const ctrlKey = IS_APPLE ? '⌘' : 'CTRL+';
 
 export const STATIC_DOMAIN = import.meta.env.VITE_STATIC_DOMAIN;
+export const DOMAIN = import.meta.env.VITE_DOMAIN;
