0a54187aa58ad6ef6dbf2c3350d6a133c98ab1a2
Karar Al-Remahy <alremahykarar@ymail.com>
2025-06-27 21:58:48 +0200

    fix yjs problem

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			yjs: path.resolve('./node_modules/yjs/dist/yjs.mjs'),
+			yjs: path.resolve('./node_modules/yjs/src/index.js'),
 			'@lexical/yjs': path.resolve('./node_modules/@lexical/yjs/LexicalYjs.mjs'),
 			'@lexical/clipboard': path.resolve('./node_modules/@lexical/clipboard/LexicalClipboard.mjs'),
 			'@lexical/code': path.resolve('./node_modules/@lexical/code/LexicalCode.mjs'),
