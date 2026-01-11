b3293b270f11f6023d8576835b30aced9e537847
Karar Al-Remahy <alremahykarar@ymail.com>
2026-01-11 14:56:38 +0100

    move cachebust to utils

---
+++ src/lib/utils/cacheBust.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Map<string, number>} cache
+ * @param {string} title
+ */
+export const getShouldCacheBust = (cache, title, cacheTTL = 21_600_000) => {
+	let cacheBust = true;
+
+	const ratelimit = cache.get(title);
+	if (ratelimit && ratelimit > Date.now()) {
+		cacheBust = false;
+	} else {
+		cache.set(title, Date.now() + cacheTTL);
+	}
+
+	return cacheBust;
+};
