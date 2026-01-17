b5e1e360a453ae5bb00627735a9225b7aaa70ba2
Karar Al-Remahy <alremahykarar@ymail.com>
2026-01-17 03:08:56 +0100

    fix: Refactor search API endpoint

--- src/routes/search/+page.server.js
+++ src/routes/search/+page.server.js
@@ -1,13 +1,6 @@
-import prisma from '$lib/prisma';
-import { getCacheURL } from '$lib/utils/getCacheURL';
+import { _getSearch } from '../api/search/+server.js';
 
-/**
- * @typedef {{ type?: 'content', rawTitle: string, title: string, lastUpdated: Date, id: string }} QueryResult
- */
 
-/**
- * @returns {Promise<{ results: QueryResult[] }>}
- */
 export const load = async ({ url }) => {
 	const rawQuery = url.searchParams.get('query');
 
@@ -17,81 +10,5 @@ export const load = async ({ url }) => {
 
 	const query = rawQuery.trim();
 
-	const rawRawTitleContains = prisma.yPost.findMany({
-		where: {
-			rawTitle: {
-				contains: query,
-				mode: 'insensitive',
-			},
-		},
-		select: {
-			id: true,
-			rawTitle: true,
-			lastUpdated: true,
-			title: true,
-		},
-	});
-
-	const rawMetadataUserName = prisma.yPost.findMany({
-		where: {
-			postUpdates: {
-				some: {
-					metadata: {
-						user: {
-							name: query.toLowerCase(),
-						},
-					},
-				},
-			},
-		},
-		select: {
-			id: true,
-			rawTitle: true,
-			lastUpdated: true,
-			title: true,
-		},
-	});
-
-	const rawContentUserName = prisma.content.findMany({
-		where: {
-			OR: [
-				{
-					author: {
-						name: query.toLowerCase(),
-					},
-				},
-				{
-					name: {
-						contains: query.toLowerCase(),
-						mode: 'insensitive',
-					},
-				},
-			],
-		},
-	});
-
-	/** @type {QueryResult[]} */
-	let results = [];
-	const [rawTitleContains, metadataUserName, contentUserName] = await Promise.all([
-		rawRawTitleContains,
-		rawMetadataUserName,
-		rawContentUserName,
-	]);
-
-	results.push(...rawTitleContains);
-	results.push(...metadataUserName);
-
-	for (let index = 0; index < contentUserName.length; index++) {
-		const { name, hash, createdTimestamp, id } = contentUserName[index];
-
-		results.push({
-			type: 'content',
-			lastUpdated: createdTimestamp,
-			rawTitle: name,
-			title: getCacheURL(hash, name).toString(),
-			id,
-		});
-	}
-
-	return { results: results.flat() };
+	return _getSearch(query);
 };
