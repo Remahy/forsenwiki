e9eeb9e5782727d1d5949167677ac46cb0e4b700
Karar Al-Remahy <alremahykarar@ymail.com>
2026-03-22 15:11:59 +0100

    fix: Remove redundant browse endpoint

--- src/lib/api/browse/index.js
+++
@@ -1,8 +0,0 @@
-const headers = new Headers({ 'content-type': 'application/json' });
-
-/**
- * @param {string} cursor YPost id
- */
-export const browseRequest = async (cursor) => {
-	return fetch(`/api/browse?cursor=${cursor}`, { method: 'GET', headers });
-};
