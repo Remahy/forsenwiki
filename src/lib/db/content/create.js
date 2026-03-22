339c5ec4274324274f9ebe2a63c324980a1900b9
Karar Al-Remahy <alremahykarar@ymail.com>
2026-03-22 01:55:15 +0100

    add type column to content

--- src/lib/db/content/create.js
+++ src/lib/db/content/create.js
@@ -5,15 +5,16 @@ import prisma from '$lib/prisma';
  */
 
 /**
- * @param {{ name: string, hash: string, authorId: string, contentType: string, metadata: any }} CreateContentArg
+ * @param {{ name: string, hash: string, authorId: string, type: string, contentType: string, metadata: any }} CreateContentArg
  * @param {PrismaTransaction} tx
  */
-export const createContent = async ({ name, hash, authorId, contentType, metadata }, tx = prisma) => {
+export const createContent = async ({ name, hash, authorId, type, contentType, metadata }, tx = prisma) => {
 	return tx.content.create({
 		data: {
 			name,
 			hash,
 			authorId,
+			type,
 			contentType,
 			metadata,
 		},
