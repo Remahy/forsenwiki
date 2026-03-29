f91ba9ff5562f173581c830dec5ac1417e57e166
Karar Al-Remahy <alremahykarar@ymail.com>
2026-03-30 01:38:04 +0200

    fix spinner being used inside p element

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -4,7 +4,7 @@
 	let { size = undefined } = $props();
 </script>
 
-<div role="status" class="inline-block animate-spin">
+<span role="status" class="inline-block animate-spin">
 	<LoaderCircleIcon {size} />
 	<span class="sr-only">Loading...</span>
-</div>
+</span>
