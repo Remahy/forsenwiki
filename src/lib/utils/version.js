4536cb0f632bd718045012273a7ca744059f6b07
Karar Remahy <alremahykarar@ymail.com>
2026-07-12 03:32:47 +0200

    dev: Development fixes (#213)

--- src/lib/utils/version.js
+++ src/lib/utils/version.js
@@ -1,2 +1,10 @@
-import v from '/version?raw';
-export const version = v.trim().replace(/[\n]+/g, '-').replace(/ /g, '-');
+let version = '';
+
+if (import.meta.env.PROD && !version) {
+	(async () => {
+		const v = await import('/version?raw');
+		version = v.default.trim().replace(/[\n]+/g, '-').replace(/ /g, '-');
+	})();
+}
+
+export { version };
