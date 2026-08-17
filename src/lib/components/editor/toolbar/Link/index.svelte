50f2925a5e47db4c209495803450824f90a2bf92
Karar Al-Remahy <alremahykarar@ymail.com>
2026-08-17 02:56:14 +0200

    add button for link unlinking

--- src/lib/components/editor/toolbar/Link/index.svelte
+++ src/lib/components/editor/toolbar/Link/index.svelte
@@ -1,4 +1,8 @@
 <script>
+	import { $toggleLink as toggleLink } from '@lexical/link';
+	import { getEditor } from 'svelte-lexical';
+	import { UnlinkIcon } from '@lucide/svelte';
+	import Button from '$lib/components/Button.svelte';
 	import { $isALinkNode as isALinkNode } from '$lib/lexical/custom';
 	import Title from '../components/Title.svelte';
 	import EditLink from './EditLink.svelte';
@@ -7,11 +11,26 @@
 	 * @type {{ selectedNode: LexicalNode | null }}
 	 */
 	let { selectedNode = null } = $props();
+
+	const editor = $derived(getEditor());
+
+	const onClickUnlink = () => {
+		editor.update(() => {
+			if (isALinkNode(selectedNode)) {
+				selectedNode.selectEnd();
+				toggleLink(null);
+			}
+		});
+	};
 </script>
 
 {#if isALinkNode(selectedNode)}
 	<div class="flex w-full flex-col justify-items-stretch">
-		<Title {selectedNode} text="Link" />
+		<Title {selectedNode} text="Link">
+			<Button class="m-0! rounded-none! p-0!" onclick={onClickUnlink} title="Unlink">
+				<UnlinkIcon />
+			</Button>
+		</Title>
 
 		<div class="min-h-32 p-2">
 			<EditLink {selectedNode} />
