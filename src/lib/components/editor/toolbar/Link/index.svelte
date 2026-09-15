bf9f8a1342b0625c9b2c77f1017b5f633c413f8a
Karar Al-Remahy <alremahykarar@ymail.com>
2026-09-15 22:25:15 +0200

    fix: Show ALinkNode in tree when selecting closest child text node

--- src/lib/components/editor/toolbar/Link/index.svelte
+++ src/lib/components/editor/toolbar/Link/index.svelte
@@ -13,6 +13,9 @@
 	 */
 	let { selectedNode = null } = $props();
 
+	/** @type {import('$lib/lexical/custom').ALinkNode | undefined} */
+	let selectedLink = $state();
+
 	const editor = $derived(getEditor());
 
 	const onClickUnlink = () => {
@@ -23,12 +26,54 @@
 			}
 		});
 	};
+
+	const updateToolbar = () => {
+		editor.read(() => {
+			if (!selectedNode) {
+				selectedLink = undefined;
+				return;
+			}
+
+			if (!selectedNode.isAttached()) {
+				return;
+			}
+
+			if (isALinkNode(selectedNode)) {
+				selectedLink = selectedNode;
+				return;
+			}
+
+			const parents = selectedNode.getParents();
+
+			const closestALinkNodeIndex = parents.findIndex((node) => isALinkNode(node));
+			if (closestALinkNodeIndex === -1) {
+				selectedLink = undefined;
+				return;
+			}
+
+			if (closestALinkNodeIndex > 1) {
+				selectedLink = undefined;
+				return;
+			}
+
+			const closestALinkNode = /** @type {import('$lib/lexical/custom').ALinkNode} */ (
+				parents[closestALinkNodeIndex]
+			);
+
+			selectedLink = closestALinkNode;
+		});
+	};
+
+	$effect(() => {
+		updateToolbar();
+		() => [selectedNode];
+	});
 </script>
 
-{#if isALinkNode(selectedNode)}
+{#if selectedLink && isALinkNode(selectedLink)}
 	<Wrapper>
 		{#snippet title()}
-			<Title {selectedNode} text="Link">
+			<Title selectedNode={selectedLink} text="Link">
 				<Button class="m-0! rounded-none! p-0!" onclick={onClickUnlink} title="Unlink">
 					<UnlinkIcon />
 				</Button>
@@ -37,7 +82,7 @@
 
 		{#snippet content()}
 			<div class="p-2">
-				<EditLink {selectedNode} />
+				<EditLink selectedNode={selectedLink} />
 			</div>
 		{/snippet}
 	</Wrapper>
